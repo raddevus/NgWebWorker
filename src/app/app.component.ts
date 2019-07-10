3093151df8e81427206697088657b7cc44a56629
raddevus <1radeutsch1@gmail.com>
2019-07-10 11:48:15 -0400

    Added constructor to app.component and moved template code to it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,20 @@ export class AppComponent {
   longProcessOutput : string = "Long\nprocess\noutput\nwill\nappear\nhere\n";
   fibCalcStartVal : number
   
+  constructor(){
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker('./app.worker', { type: 'module' });
+      worker.onmessage = ({ data }) => {
+        this.longProcessOutput += `page got message: ${data}` + "\n";
+      };
+      worker.postMessage('hello');
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
+
   longLoop(){
     this.longProcessOutput = "";
     for (var x = 1; x <=1000000000;x++){
